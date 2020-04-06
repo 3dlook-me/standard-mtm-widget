3eb254a29684e4987ddc1b67a514c1c9a21906d3
Krivokon <vladislav.krivokon@3dlook.me>
2020-04-06 15:30:34 +0300

    refactor: update state for weight screen

--- src/containers/WeightContainer/WeightContainer.jsx
+++ src/containers/WeightContainer/WeightContainer.jsx
@@ -174,18 +174,18 @@ class WeightContainer extends Component {
       email,
     } = this.props;
 
+    await this.flow.updateState({
+      status: 'set metadata',
+      gender,
+      height,
+      units,
+      email,
+      ...(weight && { weight }),
+    });
+
     if (isMobile) {
       route('/tutorial', false);
     } else {
-      await this.flow.updateState({
-        status: 'set metadata',
-        gender,
-        height,
-        units,
-        email,
-        ...(weight && { weight }),
-      });
-
       route('/qrcode', false);
     }
   }

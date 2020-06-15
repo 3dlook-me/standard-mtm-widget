cb127d6765a98b3156c01c9959b94ee7ecc3cf69
Krivokon <vladislav.krivokon@3dlook.me>
2020-06-15 13:05:32 +0300

    refactor: final voice after preloader start

--- src/containers/Upload/Upload.jsx
+++ src/containers/Upload/Upload.jsx
@@ -153,9 +153,7 @@ class Upload extends Component {
 
     if (isTableFlow) {
       if (camera) {
-        if (hardValidation.front && !hardValidation.side) {
-          setCamera(null);
-        } else {
+        if (!(hardValidation.front && !hardValidation.side)) {
           this.triggerSideImage();
         }
       }
@@ -177,11 +175,14 @@ class Upload extends Component {
       isMobile,
       setHeaderIconsStyle,
       setCamera,
+      isTableFlow,
     } = this.props;
 
     setHeaderIconsStyle('default');
 
-    setCamera(null);
+    if (!isTableFlow) {
+      setCamera(null);
+    }
 
     if (isMobile) {
       this.unsubscribe = store.subscribe(() => {
@@ -195,6 +196,12 @@ class Upload extends Component {
     }
 
     addSideImage(file);
+  }
+
+  turnOffCamera = () => {
+    const { setCamera } = this.props;
+
+    setCamera(null);
   }
 
   /**
@@ -700,22 +707,6 @@ class Upload extends Component {
                 </div>
               )}
 
-              <div className="upload__block">
-                <div className="upload__files">
-                  {camera ? (
-                    <Camera
-                      type={camera}
-                      gender={gender}
-                      saveFront={this.saveFrontFile}
-                      saveSide={this.saveSideFile}
-                      isTableFlow={isTableFlow}
-                      hardValidation={hardValidation}
-                      disableTableFlow={this.disableTableFlow}
-                    />
-                  ) : null}
-                </div>
-              </div>
-
             </div>
             <div className="screen__footer">
               <button
@@ -746,8 +737,20 @@ class Upload extends Component {
         {/* ) : null} */}
 
         <Preloader isActive={isPending} status={sendDataStatus} isMobile={isMobile} />
-      </div>
 
+        {camera ? (
+          <Camera
+            type={camera}
+            gender={gender}
+            saveFront={this.saveFrontFile}
+            saveSide={this.saveSideFile}
+            isTableFlow={isTableFlow}
+            hardValidation={hardValidation}
+            disableTableFlow={this.disableTableFlow}
+            turnOffCamera={this.turnOffCamera}
+          />
+        ) : null}
+      </div>
     );
   }
 }

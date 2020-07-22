a63a014eb90d5f7f930efd7b76708c272f96f04f
Krivokon <vladislav.krivokon@3dlook.me>
2020-07-22 10:35:00 +0300

    improvement: update example photos

--- src/components/ImageExample/ImageExample.jsx
+++ src/components/ImageExample/ImageExample.jsx
@@ -2,8 +2,8 @@ import { h, Component } from 'preact';
 import classNames from 'classnames';
 
 import './ImageExample.scss';
-import exampleSide from '../../images/HV-side.jpg';
-import exampleFront from '../../images/HV-front.jpg';
+import exampleSide from '../../images/HV-side.png';
+import exampleFront from '../../images/HV-front.png';
 
 /**
  * Help component.

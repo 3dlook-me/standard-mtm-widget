b947d7d791306c61bd3f57a190794fb9af39b86b
Krivokon <vladislav.krivokon@3dlook.me>
2020-08-13 14:48:08 +0300

    refactor: remove comments

--- src/shopify-initializer.js
+++ src/shopify-initializer.js
@@ -20,16 +20,6 @@ import SaiaButton from './button';
     parentDiv.insertBefore(cont, cartAdd);
   }
 
-  // test
-  // const button = new SaiaButton({
-  //   key: API_KEY,
-  //   widgetUrl: WIDGET_HOST,
-  //   photosFromGallery: true,
-  //   brand: 'butter denim midrise',
-  //   bodyPart: 'bottom',
-  // });
-
-  // prod
   const button = new SaiaButton({
     key: API_KEY,
     widgetUrl: WIDGET_HOST,

199b8a3e70d138f95a689d4b0418d74ae8d61ea4
Anatoliy Pisarivskiy <anatoliy@3dlook.me>
2020-08-27 17:54:54 +0300

    refactor: del SoftValidation container

--- src/containers/index.js
+++ src/containers/index.js
@@ -8,11 +8,9 @@ export * from './Tutorial';
 export * from './Upload';
 export * from './QRCodeHelp';
 export * from './Results';
-// export * from './SoftValidation';
 export * from './HardValidation';
 export * from './NotFound';
 export * from './MobileFlow';
 export * from './Browser';
 export * from './CameraModeSelection';
 export * from './HowToTakePhotos';
-
